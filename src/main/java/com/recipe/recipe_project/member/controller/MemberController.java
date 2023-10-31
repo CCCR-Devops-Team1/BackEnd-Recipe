717fc4eaa9c11a57e4817c2fda5db85fcd75c712
wooyong99 <wulovesk@naver.com>
2023-10-31 13:12:13 +0900

    refactor : member 엔티티 생성 함수 분리

--- src/main/java/com/recipe/recipe_project/member/controller/MemberController.java
+++ src/main/java/com/recipe/recipe_project/member/controller/MemberController.java
@@ -13,9 +13,7 @@ import jakarta.validation.Valid;
 import java.security.Principal;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,7 +26,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
-    private final RedisTemplate<String, String> redisTemplate;
     private final MemberRepository memberRepository;
 
     @PostMapping("/user/signup")
@@ -55,30 +52,27 @@ public class MemberController {
     @PostMapping(value = "/user/login")
     public ResponseDto login(@Valid @RequestBody LoginDto loginDto, BindingResult bindingResult) {
         List<String> error_list = Validation.getValidationError(bindingResult);
-
         if (!error_list.isEmpty()) {
             return new ResponseDto(error_list);
         }
         TokenDto tokenDto = memberService.login(loginDto);
-        // Security Context에 저장된 사용자 확인
-        System.out.println(SecurityContextHolder.getContext().getAuthentication().getName());
-        System.out.println(
-                redisTemplate.opsForValue().get(SecurityContextHolder.getContext().getAuthentication().getName()));
         return new ResponseDto(tokenDto);
     }
 
     @PostMapping("/user/logout")
     public ResponseDto logout(Principal principal, @RequestBody TokenDto tokenDto) {
         memberService.logout(tokenDto.getRefresh_token());
-        System.out.println(
-                redisTemplate.opsForValue().get(SecurityContextHolder.getContext().getAuthentication().getName()));
         return new ResponseDto(ResponseStatus.SUCCESS);
+    }
+
+    @PostMapping("/auth/reissue")
+    public ResponseDto<TokenDto> reissue(@RequestBody TokenDto tokenDto) {
+        return new ResponseDto<>(memberService.reissueToken(tokenDto.getRefresh_token()));
     }
 
     @PutMapping("/user")
     public ResponseDto updatePw(Principal principal, @RequestBody Member memberDto) {
         memberService.updatePw(memberDto, principal.getName());
-
         return new ResponseDto(memberRepository.findByAccount(principal.getName()).get());
     }
 
--- src/main/java/com/recipe/recipe_project/member/service/MemberService.java
+++ src/main/java/com/recipe/recipe_project/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.recipe.recipe_project.member.service;
 
 import static com.recipe.recipe_project.global.dto.response.ResponseStatus.LOGIN_FAIL;
+import static com.recipe.recipe_project.global.dto.response.ResponseStatus.NOT_EXIST_REFRESH_JWT;
 import static com.recipe.recipe_project.global.dto.response.ResponseStatus.NOT_FOUND_MEMBER;
 import static com.recipe.recipe_project.global.dto.response.ResponseStatus.POST_PASSWORD_INCORRECT;
 import static com.recipe.recipe_project.global.dto.response.ResponseStatus.SIGNUP_DUPLI_MEMBER;
@@ -15,6 +16,7 @@ import com.recipe.recipe_project.member.repository.MemberRepository;
 import com.recipe.recipe_project.notice.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -27,6 +29,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+
+    private final AuthenticationManager authenticationManager;
     private final MemberRepository memberRepository;
     private final ArticleRepository articleRepository;
     private final PasswordEncoder passwordEncoder;
@@ -38,11 +42,7 @@ public class MemberService {
         if (memberRepository.existsByAccount(account)) {
             throw new BaseException(SIGNUP_DUPLI_MEMBER);
         }
-        Member member = Member.builder()
-                .account(account)
-                .pw(passwordEncoder.encode(pw))
-                .build();
-        memberRepository.save(member);
+        memberRepository.save(dtoToEntity(account, pw));
     }
 
     public TokenDto login(LoginDto loginDto) {
@@ -84,14 +84,9 @@ public class MemberService {
         }
     }
 
+    @Transactional
     public void updatePw(Member memberDto, String account) {
         Member member = memberRepository.findByAccount(account).get();
-//    Authentication authentication = customAuthenticationProvider.authenticate(
-//            new UsernamePasswordAuthenticationToken(account, memberDto.getPw()));
-//    SecurityContext sc = SecurityContextHolder.createEmptyContext();
-//    sc.setAuthentication(authentication);
-//    SecurityContextHolder.setContext(sc);
-        System.out.println(memberDto.getPw());
         member.setPw(passwordEncoder.encode(memberDto.getPw()));
         memberRepository.save(member);
     }
@@ -103,7 +98,32 @@ public class MemberService {
         memberRepository.delete(member);
     }
 
+    public TokenDto reissueToken(String refreshToken) throws BaseException {
+        // Refresh Token 검증
+        jwtTokenProvider.validateToken(refreshToken);
+        // Access Token 에서 User 가져옴
+        Authentication authentication = jwtTokenProvider.getAuthentication(refreshToken);
+        // Redis에서 저장된 Refesh Token 값 가져옴
+        String redisRefreshToken = redisTemplate.opsForValue().get(authentication.getName());
+        if (!redisRefreshToken.equals(refreshToken)) {
+            throw new BaseException(NOT_EXIST_REFRESH_JWT);
+        }
+        // 토큰 재발행
+        TokenDto tokenDto = new TokenDto(
+                jwtTokenProvider.createAccessToken(authentication.getName()),
+                jwtTokenProvider.createRefreshToken(authentication.getName())
+        );
+        return tokenDto;
+    }
+
     public Member getUser(String name) {
         return memberRepository.findByAccount(name).get();
+    }
+
+    private Member dtoToEntity(String account, String pw) {
+        return Member.builder()
+                .account(account)
+                .pw(passwordEncoder.encode(pw))
+                .build();
     }
 }
