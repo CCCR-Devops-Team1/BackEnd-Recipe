6b438439470e11db49999c50b7043f1dc7aa53e9
wooyong99 <wulovesk@naver.com>
2023-10-31 18:02:15 +0900

    fix : cors 에러 해결

--- src/main/java/com/recipe/recipe_project/member/controller/MemberController.java
+++ src/main/java/com/recipe/recipe_project/member/controller/MemberController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MemberController {
     private final MemberService memberService;
     private final MemberRepository memberRepository;
